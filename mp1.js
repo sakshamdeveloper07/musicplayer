34332c74396b6ddbd6a5777ef9374f136e9afb05
sakshamsingh <142484190+sakshamdeveloper07@users.noreply.github.com>
2023-09-23 01:11:58 +0530

    Add files via upload

---
+++ mp1.js
@@ -0,0 +1,157 @@
+new Vue({
+    el: "#app",
+    data() {
+      return {
+        audio: null,
+        circleLeft: null,
+        barWidth: null,
+        duration: null,
+        currentTime: null,
+        isTimerPlaying: false,
+        tracks: [
+          {
+            name: "Indila ",
+            artist: "Andrew TAttee",
+            cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxYCq2jAjXNmp_Cgd1UZCUk7C86nCiKK7DjooUUPnRWaldl60WyRDLqD6AikOee0Th-ZY&usqp=CAU",
+            source: "https://jesusful.com/wp-content/uploads/music/2022/07/Indila_-_Tourner_Dans_Le_Vide_(Jesusful.com).mp3",
+            url: " ",
+            favorited: false
+          },
+          {
+            name: "Try ME",
+            artist: "gita di machine(karan aujla)",
+            cover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqAZ8_CW2FJebViuUgfY5n0z4C9eAtX8a4AQ&usqp=CAU",
+            source: "https://cdnsongs.com/music/data/Punjabi/202308/Making_Memories/128/Try_Me.mp3",
+            url: " ",
+            favorited: true
+          },
+        
+        ],
+        
+        currentTrack: null,
+        currentTrackIndex: 0,
+        transitionName: null
+      };
+    },
+    methods: {
+      play() {
+        if (this.audio.paused) {
+          this.audio.play();
+          this.isTimerPlaying = true;
+        } else {
+          this.audio.pause();
+          this.isTimerPlaying = false;
+        }
+      },
+      generateTime() {
+        let width = (100 / this.audio.duration) * this.audio.currentTime;
+        this.barWidth = width + "%";
+        this.circleLeft = width + "%";
+        let durmin = Math.floor(this.audio.duration / 60);
+        let dursec = Math.floor(this.audio.duration - durmin * 60);
+        let curmin = Math.floor(this.audio.currentTime / 60);
+        let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+        if (durmin < 10) {
+          durmin = "0" + durmin;
+        }
+        if (dursec < 10) {
+          dursec = "0" + dursec;
+        }
+        if (curmin < 10) {
+          curmin = "0" + curmin;
+        }
+        if (cursec < 10) {
+          cursec = "0" + cursec;
+        }
+        this.duration = durmin + ":" + dursec;
+        this.currentTime = curmin + ":" + cursec;
+      },
+      updateBar(x) {
+        let progress = this.$refs.progress;
+        let maxduration = this.audio.duration;
+        let position = x - progress.offsetLeft;
+        let percentage = (100 * position) / progress.offsetWidth;
+        if (percentage > 100) {
+          percentage = 100;
+        }
+        if (percentage < 0) {
+          percentage = 0;
+        }
+        this.barWidth = percentage + "%";
+        this.circleLeft = percentage + "%";
+        this.audio.currentTime = (maxduration * percentage) / 100;
+        this.audio.play();
+      },
+      clickProgress(e) {
+        this.isTimerPlaying = true;
+        this.audio.pause();
+        this.updateBar(e.pageX);
+      },
+      prevTrack() {
+        this.transitionName = "scale-in";
+        this.isShowCover = false;
+        if (this.currentTrackIndex > 0) {
+          this.currentTrackIndex--;
+        } else {
+          this.currentTrackIndex = this.tracks.length - 1;
+        }
+        this.currentTrack = this.tracks[this.currentTrackIndex];
+        this.resetPlayer();
+      },
+      nextTrack() {
+        this.transitionName = "scale-out";
+        this.isShowCover = false;
+        if (this.currentTrackIndex < this.tracks.length - 1) {
+          this.currentTrackIndex++;
+        } else {
+          this.currentTrackIndex = 0;
+        }
+        this.currentTrack = this.tracks[this.currentTrackIndex];
+        this.resetPlayer();
+      },
+      resetPlayer() {
+        this.barWidth = 0;
+        this.circleLeft = 0;
+        this.audio.currentTime = 0;
+        this.audio.src = this.currentTrack.source;
+        setTimeout(() => {
+          if(this.isTimerPlaying) {
+            this.audio.play();
+          } else {
+            this.audio.pause();
+          }
+        }, 300);
+      },
+      favorite() {
+        this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+          this.currentTrackIndex
+        ].favorited;
+      }
+    },
+    created() {
+      let vm = this;
+      this.currentTrack = this.tracks[0];
+      this.audio = new Audio();
+      this.audio.src = this.currentTrack.source;
+      this.audio.ontimeupdate = function() {
+        vm.generateTime();
+      };
+      this.audio.onloadedmetadata = function() {
+        vm.generateTime();
+      };
+      this.audio.onended = function() {
+        vm.nextTrack();
+        this.isTimerPlaying = true;
+      };
+  
+      // this is optional (for preload covers)
+      for (let index = 0; index < this.tracks.length; index++) {
+        const element = this.tracks[index];
+        let link = document.createElement('link');
+        link.rel = "prefetch";
+        link.href = element.cover;
+        link.as = "image"
+        document.head.appendChild(link)
+      }
+    }
+  });
